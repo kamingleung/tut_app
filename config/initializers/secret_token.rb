c3d3c1be98eecdbd4f7140dd1a3e4bdf99dcd69f
Ka Ming <kaming@npathway.com>
2013-03-12 14:59:40 +0800

    Initial Build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutApp::Application.config.secret_token = 'dc2410150849fd30ae6a540086058065be77203fceba5777b8c6bb0fc557f6928dfa794b3982cdf92e7f22c53fa9279c9d6c1c229e09874d861281c620b06a70'
